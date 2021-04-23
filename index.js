42d78bd5689498b37d78309072d1a7fb27009b4e
alisonarmstrong <alarmstrong15@gmail.com>
2021-04-23 09:27:50 -0600

    added inquirer prompts

---
+++ index.js
@@ -0,0 +1,110 @@
+const Manager = require("./library/manager");
+const Engineer = require("./library/engineer");
+const Intern = require("./library/intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+function employeeInfo() {
+    inquirer.prompt([
+        {
+        type: 'list',
+        message: 'what type of employee?', 
+        name: 'type',
+        choices: ['Manager', 'Engineer', 'Intern'],
+        },
+    ]).then(val => {
+        if (val.name === 'Manager') {
+            managerInfo();
+        }
+        else if (val.name === 'Engineer') {
+            engineerInfo();
+        } 
+        (val.name === 'Intern') {
+            internInfo();
+        }
+    })
+};
+
+function managerInfo() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Managers Name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Managers ID:',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Managers Email:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Managers Office Number:',
+            name: 'office number',
+        },
+    ])
+}
+
+function engineerInfo() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Engineers Name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Engineers ID:',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Engineers Email:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Engineers GitHub:',
+            name: 'GitHub',
+        },
+    ])
+}
+
+function internInfo() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Intern Name:',
+            name: 'name',
+        },
+        {
+            type: 'input',
+            message: 'Intern ID:',
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: 'Intern Email:',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Intern School:',
+            name: 'school',
+        },
+    ])
+}
+
+function generateHTML(fileName, data) {
+    fs.writeFile(fileName, data, "utf8", function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log("You have successfully enetered your employee informaion")
+    })
+}
---
+++ source/template.js
@@ -0,0 +1,11 @@
+const generateTeam = team => {
+    const generateManager = manager => {}
+    return `<div>HTML ${manager.getName()}</div>`
+};
+}
+
+
+
+module.exports = team => {
+    return ``
+}
